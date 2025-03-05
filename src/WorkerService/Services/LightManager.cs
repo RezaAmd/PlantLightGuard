d4e8248720daa29b41a90f249113a586e3e8c628
Reza Ahmadi <rezaahmadidvlp@gmail.com>
2025-03-05 17:46:45 +0330

    ✨ log schedules list.

--- src/WorkerService/Services/LightManager.cs
+++ src/WorkerService/Services/LightManager.cs
@@ -16,6 +16,9 @@ namespace WorkerService.Services
         public async Task RunByScheduleAsync(LampScheduleModel lampModel,
             CancellationToken cancellationToken = default)
         {
+            // Print schedules.
+            PrintTimeSchedules(lampModel.TurnOnPeriod);
+
             // Prepare pin for output on board.
             PreparePinForOutput(lampModel.Pin);
 
@@ -89,10 +92,19 @@ namespace WorkerService.Services
             _logger.LogInformation("The lamp turned off.");
         }
 
-        public async Task WaitAMinuteAsync(CancellationToken cancellationToken = default)
+        private async Task WaitAMinuteAsync(CancellationToken cancellationToken = default)
         {
             _logger.LogInformation("Sleep for 1 minute.");
             await Task.Delay(60_000, cancellationToken);
+        }
+
+        private void PrintTimeSchedules(List<TimeDurationModel> Schedule)
+        {
+            _logger.LogInformation($"{Schedule.Count} Schedules exist.");
+            foreach (var timeDuration in Schedule)
+            {
+                _logger.LogInformation($"From {timeDuration.Start} To {timeDuration.End}");
+            }
         }
         #endregion
     }
