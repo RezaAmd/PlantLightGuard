d2246610da4af011980b1cba6e6529b1f4876731
Reza Ahmadi <rezaahmadidvlp@gmail.com>
2025-03-05 17:50:22 +0330

    ✨ Map appsetting json into AppSettingModel in startup.

--- src/WorkerService/Program.cs
+++ src/WorkerService/Program.cs
@@ -1,9 +1,14 @@
 using WorkerService;
+using WorkerService.Models;
 using WorkerService.Services;
 
 var builder = Host.CreateApplicationBuilder(args);
 builder.Services.AddSingleton<LightManager>();
+
+builder.Services.Configure<AppSettingModel>(builder.Configuration.GetSection("Setting"));
+
 builder.Services.AddHostedService<Worker>();
+
 
 var host = builder.Build();
 await host.RunAsync();
